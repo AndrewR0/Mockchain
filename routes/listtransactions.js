ec681b452ae182752839296cefcca566d2490476
Andrew <andrewrednikov@outlook.com>
2022-02-25 21:00:22 -0500

    Changed spacing

--- routes/listtransactions.js
+++ routes/listtransactions.js
@@ -2,36 +2,26 @@
 
 // * Lists all transactions in the system, not currently on blocks.
 
-
-
 // * Imports
 
 const Transaction = require("../src/transaction");
 
-
-
 function listtransactions(app) {
 
     // List all transactions
-
     app.get("/listtransactions", function (request, response) {
-
-        // Iterate through all transactions and create a response string
 
         let txStr = "";
 
+        // Iterate through all transactions and create a response string
         for (let i = 0; i < global.transactions.length; i++) {
 
             txStr += global.transactions[i].prettify();
 
         }
 
-
-
         // Send the response for creating a new transaction
-
         response
-
             .status(200) // HTTP status code 200: OK
 
             .send(txStr); // Response message
@@ -39,7 +29,5 @@ function listtransactions(app) {
     });
 
 }
-
-
 
 module.exports = listtransactions;
